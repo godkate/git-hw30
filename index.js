629ddfff23aa0158f03ef290cee90f51edf96fe9
godkate <bogodul883@gmail.com>
2022-12-23 20:26:41 -0500

    Added some changes

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const categories = document.querySelector('.categories');
+const listOfGoods = document.querySelector('.cat-content');
+const modal = document.querySelector('.modal__window');
+const buy = document.querySelectorAll('.button');
+const ok = document.querySelector('.sold');
 
-function addHide () {
+function addHideCategory () {
     const goods = document.querySelectorAll('.cat-content');
     goods.forEach((content) => {
         content.classList.add('hide');
     });
 }
+
+function addHideProduct () {
+    const products = document.querySelectorAll('.product');
+    products.forEach((prod) => {
+        prod.classList.add('hide');
+    });
+}
+
 categories.addEventListener('click', ({ target }) => {
         if (target.matches('a') && target.dataset.target) {
             const id = target.dataset.target;
             const cat = document.querySelector(`#${id}`);
-            addHide();
+            addHideCategory();
+            addHideProduct();
             cat.classList.remove('hide');
-
         }
+});
+
+listOfGoods.addEventListener('click', ({ target }) => {
+    if (target.matches('a') && target.dataset.target) {
+        const id = target.dataset.target;
+        const product = document.querySelector(`#${id}`);
+        addHideProduct();
+        product.classList.remove('hide');
+    }
 });
 
 
 
+buy.forEach((button) => {
+    button.onclick = (event) => {
+        event.preventDefault();
+        modal.classList.remove('hide__modal');
+    }
+});
+ok.onclick = () => {
+  modal.classList.add('hide__modal');
+  addHideProduct();
+  addHideCategory();
+};
 
